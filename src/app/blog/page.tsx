eccead2120a3601db411fbfc614bae3fc3db5b9c
Bounty123-creator <juliangomberg88@gmail.com>
2024-07-25 13:45:13 +0200

    Finished

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,54 @@
-import styles from "./page.module.css"
+import styles from "./page.module.css";
+import Image from "next/image";
+
+type postType = {
+    date: string,
+    text: string,
+    subtext: string,
+}
 
 export default function Page() {
-    return (
-        <div className={styles.main}>
+  return (
+    <div className={styles.main}>
+        <h2>Outfit Anyone AI Blog</h2>
+
+        <div className={styles.postsContainer}>
+            <Post date="2023-12-23" text="Outfit Anyone: Transforming
+              Virtual Try-On" subtext="Outfit Anyone introduction"></Post>
 
         </div>
-    )
+    </div>
+  );
+}
+
+function Post({date, text, subtext}: postType) {
+  return (
+    <a href="">
+        <div className={styles.post}>
+          <div className={styles.postImage}>
+            <Image
+              src="/blog.jpg"
+              fill={true}
+              alt="Test"
+              objectFit="cover"
+            ></Image>
+          </div>
+
+          <div className={styles.postText}>
+            <p>
+              <small>{date}</small>
+            </p>
+            <p>
+              {text}
+            </p>
+            <p>
+              <small>
+                {subtext}
+              </small>
+            </p>
+          </div>
+        </div>
+    </a>
+
+  );
 }
